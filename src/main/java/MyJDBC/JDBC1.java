223532840e35a5122c5f17a93491dc4dd723d237
Akshaypatel1812 <pak180301@gmail.com>
2024-01-23 14:31:09 +0530

    modify JDBC 1

--- src/main/java/MyJDBC/JDBC1.java
+++ src/main/java/MyJDBC/JDBC1.java
@@ -1,10 +1,17 @@
  package MyJDBC;
 
 public class JDBC1 {
+	
+	public static int sum(int a,int b) {
+		return a+b;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Jay Shree Ram");
         System.out.println("Jay Shree Ram");
+        
+        System.out.println(sum(2,3));
+        
 	}
 
 }
